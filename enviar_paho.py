28f8894d1f9e9c2647eff4809808b7baa363a489
ETR <49214324+etorresr@users.noreply.github.com>
2022-08-18 14:47:11 -0500

    Update enviar_paho.py

--- enviar_paho.py
+++ enviar_paho.py
@@ -7,7 +7,6 @@ def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
 
 client = paho.Client()
-#client.username_pw_set("etorresr", "G4t0")
 client.on_publish = on_publish
 client.connect("broker.mqttdashboard.com", 1883)
 client.loop_start()
@@ -19,9 +18,6 @@ while True:
     print(temperature)
     print('The CPU usage is: ', psutil.cpu_percent(4))
     print("Number of cores in system", psutil.cpu_count())
-    print("CPU Statistics", psutil.cpu_stats())
     print("CPU frequency", psutil.cpu_freq())
-    print("CPU load average 1, 5, 15 minutes", psutil.getloadavg())
     print(psutil.virtual_memory())
-    print(psutil.sensors_battery())
     time.sleep(30)
